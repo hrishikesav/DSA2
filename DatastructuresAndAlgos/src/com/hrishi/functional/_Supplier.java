77725435312235e43eaaf77ed2b301fc150f6faa
hrishikesav <hrishikesav@gmail.com>
2020-03-24 21:09:38 +0530

    Update _Supplier.java

--- DatastructuresAndAlgos/src/com/hrishi/functional/_Supplier.java
+++ DatastructuresAndAlgos/src/com/hrishi/functional/_Supplier.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 public class _Supplier {
 
 	public static void main(String[] args) {
+		
 		Supplier<String> getDbUrl = () -> "jdbc://localhost:8978";
 		System.out.println(getDbUrl.get());
 	}
