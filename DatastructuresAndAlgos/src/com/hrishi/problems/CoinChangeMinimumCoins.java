fc98ce75dd4e46c6e98fabf3d1348e0b57194e62
hrishikesav <hrishikesav@gmail.com>
2020-06-09 09:54:51 +0530

    CoinChange problem - To get the minimum possible coins

---
+++ DatastructuresAndAlgos/src/com/hrishi/problems/CoinChangeMinimumCoins.java
@@ -0,0 +1,44 @@
+package com.hrishi.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * This gets the minimum possible coins needed to get to the amount(num).
+ * 
+ * To get all the possible combinations to get to the amount(num) see below class:
+ * {CoinChangeAllPossibleCombinations}
+ */
+public class CoinChangeMinimumCoins {
+	
+	public static void main(String[] args) {
+
+		int[] coins = { 1, 2, 3 };
+		int num = 5;
+		List<Integer> result = new ArrayList<>();
+
+		coinChange(coins, num, num, result, coins.length - 1);
+
+	}
+
+	static void coinChange(int[] coins, int num, int remSum, List<Integer> result, int level) {
+
+		if (remSum == 0) {
+			for (int i = 0; i < result.size(); i++) {
+				System.out.print(result.get(i) + " ");
+			}
+			return;
+		}
+
+		if (remSum - coins[level] < 0 && level > 0) {
+			level--;
+		}
+
+		int nAdd = coins[level];
+		result.add(nAdd);
+		coinChange(coins, num, remSum - nAdd, result, level);
+		result.remove(result.size() - 1);
+
+	}
+
+}
