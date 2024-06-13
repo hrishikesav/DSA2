6463dcd31889ab75934d31c4c07caa15cf1203f2
Hrishi Kesav <hrishikesav@gmail.com>
2024-06-13 20:29:36 +0530

    Improved recursive version.

--- DatastructuresAndAlgos/src/com/hrishi/problems/Fibonacci.java
+++ DatastructuresAndAlgos/src/com/hrishi/problems/Fibonacci.java
@@ -3,6 +3,7 @@ package com.hrishi.problems;
 public class Fibonacci {
 
 	static int n1 = 0, n2 = 1;
+	static int i = 0;
 	
 	public static void main(String[] args) {
 		
@@ -10,7 +11,6 @@ public class Fibonacci {
 		System.out.print("0 1 ");
 		fibo(count);
 		System.out.println();
-		System.out.print("0 1 ");
 		count = 10;
 		fiboRecursion(count);
 	}
@@ -26,14 +26,21 @@ public class Fibonacci {
 		}
 	}
 	
-	static void fiboRecursion(int count) {
-		if(count > 2) {
-			int sum = n1 + n2;
-			System.out.print(sum + " ");
-			n1 = n2;
-			n2 = sum;
-			--count;
-			fiboRecursion(count);
+	static void fiboRecursion(int num) {
+		if(num > 0) {
+			if(i == 0) {
+				System.out.print(n1 + " ");
+				i++;
+			} else if (i == 1) {
+				System.out.print(n2 + " ");
+				i++;
+			} else {
+				int n3 = n1 + n2;
+				n1 = n2;
+				n2 = n3;
+				System.out.print(n3 + " ");
+			}
+			fiboRecursion(num-1);
 		}
 	}
 
